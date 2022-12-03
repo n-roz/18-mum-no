8ab5a177e77b52824dfd2b0b9cdf0778c6b06b3a
Mona Lisa <naomiroznik@gmail.com>
2022-12-03 11:23:28 -0500

    need this to test the api

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,4 +1,19 @@
 // 18.1.7
 const router = require('express').Router();
 
-const { getAllUsers, createUser, getUserById, updatUser, deleteUser } = require('../../controllers/user-controller');
+const { getAllUsers, createUser, getUserById, updateUser, deleteUser } = require('../../controllers/user-controller');
+
+// /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser)
+
+// /api/users/:id
+router
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser);
+
+module.exports = router;
